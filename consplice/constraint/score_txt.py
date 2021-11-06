144c97c894ec6d2e09a5f94a862becb6d5e2166d
Michael Cormier <cormiermichaelj@gmail.com>
2021-11-05 20:37:29 -0600

    allow multi-line headers

--- consplice/constraint/score_txt.py
+++ consplice/constraint/score_txt.py
@@ -13,22 +13,24 @@ from .utils import extract_constraint_score, get_alternative_gene_symbols
 def add_score_txt(sub_p):
 
     p = sub_p.add_parser("score-txt",
-                         help = "Add ConSplice scores to a txt variant file",
+                         help = "Add ConSplice scores to a tab-delimited txt variant file",
                          formatter_class=argparse.RawDescriptionHelpFormatter,
-                         description=("\n\t****************************\n"
-                                      "\t* Score txt with ConSplice *\n"
-                                      "\t****************************\n\n"
-                                      "\tScore a 1-based variant file in txt format with ConSplice scores\n"
+                         description=("\n\t********************************************\n"
+                                      "\t* ConSplice - Score tab-delimited txt with *\n"
+                                      "\t********************************************\n\n"
+                                      "\tScore a 1-based variant file in tab-delimited txt format with ConSplice scores\n"
                                       "\t - Expecting a tab delimited file with a header line that start with a #\n"
-                                      "\t - Only a single header line is allowed\n"
+                                      "\t - The last header line with a # will be treated as a column index based\n"
+                                      "\t    on the tab seperated names in that line.\n"
                                       "\t - The variant position will be treated as 1-based genomic position\n"
-                                      "\t - The ConSplice score will be assigned by either a gene name if \n"
-                                      "\t   provided by the user, or the max ConSplice score for that position.\n"
+                                      "\t - The ConSplice score will be assigned by either a gene name or the \n"
+                                      "\t    max ConSplice score for that position based on input parameters.\n"
 
                          )
     )
 
     req = p.add_argument_group("Required Arguments")
+    rif = p.add_argument_group("Required if by-gene")
     req.add_argument(
         "--consplice-file",
         metavar = "ConSplice Score bed File", 
@@ -77,7 +79,7 @@ def add_score_txt(sub_p):
         help="The name for the gene name/symbol column in the txt file. (Default = gene_name) (NOTE: Expecting tab delimited file with a header line)"
     )
 
-    p.add_argument(
+    rif.add_argument(
         "--alt-gene-symbol",
         metavar = "Alternative Gene Symbol File",
         default = "NA",
@@ -112,9 +114,9 @@ def add_score_txt(sub_p):
 
 def add_conSplice_score(parser, args):
 
-    print("\n\n\t****************************")
-    print("\t* Score txt with ConSplice *")
-    print("\t****************************\n\n")
+    print("\n\n\t***************************************")
+    print("\t* ConSplice - Score tab-delimited txt *")
+    print("\t***************************************\n\n")
 
 
     print(("\nInput Arguments:"
@@ -157,6 +159,35 @@ def add_conSplice_score(parser, args):
         alt_symbol_dict = get_alternative_gene_symbols(args.alt_gene_symbol)
 
 
+    print("\nChecking for header in variant file")
+    header = []
+    h_index = 0
+    with io.open(args.txt_file, "rt", encoding = "utf-8") as fh:
+        
+        header_line = fh.readline()
+
+        assert header_line[0] == "#", "\n!!ERROR!! the first line of the txt file should be a header line with a # starting the header" 
+
+        header = header_line.replace("#","").strip().split("\t")
+
+        ## Iterate over the file until the header lines starting with a # are done
+        for line in fh:
+            
+            if line[0] == "#":
+                header = line.replace("#","").strip().split("\t")
+            else:
+                break
+
+            ## update header index
+            h_index += 1
+
+        assert args.txt_chrom in header, "\n!!ERROR!! the '{}' chrom column is not in the txt file".format(args.txt_chrom)
+        assert args.txt_pos in header, "\n!!ERROR!! the '{}' position column is not in the txt file".format(args.txt_pos)
+
+        if args.score_type == "by-gene":
+            assert args.txt_gene_name in header, "\n!!ERROR!! the '{}' gene name column is not in the txt file".format(args.txt_gene_name)
+
+
     print("\nLoading ConSplice scores into an interval tree")
 
     consplice_interlap = extract_constraint_score(file_path  = args.consplice_file, 
@@ -167,30 +198,21 @@ def add_conSplice_score(parser, args):
                                                   zero_based = True if args.consplice_file.endswith(".bed") or args.consplice_file.endswith("bed.gz") else False)
 
 
+
     print("\nParsing variant file and adding ConSplice scores")
     with io.open(args.txt_file, "rt", encoding = "utf-8") as fh, io.open(args.out_file, "w") as out_fh:
         
-        header_line = fh.readline()
-
-        print("\nChecking header of the variant txt file")
-        assert header_line[0] == "#", "\n!!ERROR!! the first line of the txt file should be a header line with a # starting the header" 
-
-        header = header_line.replace("#","").strip().split("\t")
-
-        assert args.txt_chrom in header, "\n!!ERROR!! thet '{}' chrom column is not in the txt file".format(args.txt_chrom)
-        assert args.txt_pos in header, "\n!!ERROR!! thet '{}' position column is not in the txt file".format(args.txt_pos)
-        assert args.txt_gene_name in header, "\n!!ERROR!! thet '{}' gene name column is not in the txt file".format(args.txt_gene_name)
-
-        out_fh.write("#" + "\t".join(header) + "\t" + args.out_consplice_col + "\n")
-
-        for line in fh:
+        ## iterate over each variant in the file
+        for i,line in enumerate(fh):
             
-            assert line[0] != "#", "\n!!ERRORR!! too many header lines in the txt file. Only the first line should be a header line"
-
-
+            ## Write the header lines 
+            if line[0] == "#":
+                line = line.strip() + "\t{}\n".format(args.out_consplice_col) if i == h_index else line
+                out_fh.write(line)
+                continue
+            
             line_list = line.strip().split("\t")
             line_dict = dict(zip(header, line_list))
-
 
             chrom = line_dict[args.txt_chrom].replace("chr","")
             pos = int(line_dict[args.txt_pos])
@@ -223,6 +245,7 @@ def add_conSplice_score(parser, args):
             line_list.append(str(var_score))
 
             out_fh.write("\t".join(line_list) + "\n")
+
 
     print("\nConSplice scores added to '{}' output file under the '{}' column name".format(args.out_file, args.out_consplice_col))
 
